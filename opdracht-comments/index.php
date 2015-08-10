97aaa18af4ae649c263d5035325aa1b5bb702ee7
unknown <LocalAdmin@MECH-B106-05.syntraab.mechelen>
2015-08-10 13:28:48 +0200

    oplossing comments

---
+++ opdracht-comments/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$vnaam = "tony";
+$naam = "bijl";
+
+/*
+Tony Bijl
+tony.bijl59@gmail.com
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>opdracht comment</title>
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/global.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/directory.css">
+	<link rel="stylesheet" type="text/css" href="http://web-backend.local/css/facade.css">
+</head>
+<body>
+<p> <?= $vnaam ?> <?= $naam ?> </p>
+
+/
+</body>
+
+</html>
--- test.php
+++ test.php
@@ -1,3 +1,3 @@
-?php
+<?php
 echo'tweede lijn php';
-?
+?>
